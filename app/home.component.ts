d6d039e65cc272910f41210569d398d213f81266
OgheneMichael <zonalmeet@gmail.com>
2019-08-10 23:36:45 +0100

    Code for Root module routes and outlet

---
+++ app/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  // styleUrls: ['./name.component.scss'],
+  template: `
+    <div>
+      Airplane passenger app!
+    </div>
+  `
+})
+export class HomeComponent {
+  constructor() {}
+}
