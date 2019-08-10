dcb9c4f9cc170d1085d2aaa457b7a727410b4ac2
OgheneMichael <zonalmeet@gmail.com>
2019-08-10 19:05:59 +0100

    Code for Form validation and error states

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -11,12 +11,30 @@ import { Baggage } from "../../models/baggage.interface";
       {{ detail | json }}
       <div>
         Passenger name:
-        <input type="text" name="fullname" [ngModel]="detail?.fullname" />
+        <input
+          type="text"
+          name="fullname"
+          required
+          #fullname="ngModel"
+          [ngModel]="detail?.fullname"
+        />
+        <div *ngIf="fullname.errors?.required && fullname.dirty" class="error">
+          Passanger name is required
+        </div>
       </div>
 
       <div>
         Passenger ID:
-        <input type="number" name="id" [ngModel]="detail?.id" />
+        <input
+          type="number"
+          name="id"
+          required
+          #id="ngModel"
+          [ngModel]="detail?.id"
+        />
+        <div *ngIf="id.errors?.required && id.dirty" class="error">
+          Passanger ID is required
+        </div>
       </div>
 
       <div>
@@ -50,18 +68,11 @@ import { Baggage } from "../../models/baggage.interface";
             {{ item.value }}
           </option>
         </select>
-        <select name="baggage" [ngModel]="detail?.baggage">
-          <option
-            *ngFor="let item of baggage"
-            [ngValue]="item.key"
-            [selected]="item.key === detail?.baggage"
-          >
-            {{ item.value }}
-          </option>
-        </select>
       </div>
 
-      {{ form.value | json }}
+      <div>{{ form.value | json }}</div>
+      <div>Valid: {{ form.valid | json }}</div>
+      <div>Invalid: {{ form.invalid | json }}</div>
     </form>
   `
 })
